06a5ec6b2f04171e2c17c34dcb74d0be25a536c0
umangsainirke1-glitch <umangsainirke1@gmail.com>
2025-10-14 12:50:57 +0530

    Add files via upload

---
+++ 1st.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+printf("***CONVERSION OF TEMPERATURE FROM CELCIUS TO FAHRENHEIT\n");
+  float celcius, fahrenheit ;
+  printf("Enter the temperature in celcius : ");
+  scanf("%f",&celcius);
+
+  fahrenheit= (celcius * 9/5 ) +32;
+printf("Temperature in fahrenheit is : %f",fahrenheit);
+
+}
